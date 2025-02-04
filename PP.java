8d5b2a1f874f0cc967cbd63c31488a9747816aef
ztmmu <1211102370@student.mmu.edu.my>
2025-02-05 02:59:15 +0800

    pasted jea shen's PP code

--- PP.java
+++ PP.java
@@ -31,6 +31,72 @@ public class PP {
     }
 
     public void startProcess() {
-        // algorithm that will update the LinkedList of ganttChartTime, ganttChartProcess, and finishTime
+        LinkedList<Integer> remainingBurstTime = new LinkedList<>(burstTime);
+        LinkedList<Integer> tempPriority = new LinkedList<>(priority);
+        LinkedList<Integer> tempArrivalTime = new LinkedList<>(arrivalTime);
+    
+        ganttChartTime.add(0);
+        int currentTime = 0;
+        int completedProcesses = 0;
+    
+        while (completedProcesses < numberOfProcess) {
+            int highestPriorityIndex = -1;
+            int highestPriorityValue = Integer.MAX_VALUE;
+    
+            // Find the process with the highest priority that has arrived and is not yet completed
+            for (int i = 0; i < numberOfProcess; i++) {
+                if (remainingBurstTime.get(i) > 0 && tempArrivalTime.get(i) <= currentTime) {
+                    if (tempPriority.get(i) < highestPriorityValue) {
+                        highestPriorityValue = tempPriority.get(i);
+                        highestPriorityIndex = i;
+                    }
+                }
+            }
+    
+            if (highestPriorityIndex != -1) {
+                int processIndex = highestPriorityIndex;
+                int processBurstTime = remainingBurstTime.get(processIndex);
+    
+                // Execute the process for its full remaining burst time
+                remainingBurstTime.set(processIndex, 0);
+                ganttChartTime.add(ganttChartTime.getLast() + processBurstTime);
+                ganttChartProcess.add(processName.get(processIndex));
+    
+                finishTime.add(ganttChartTime.getLast());
+                completedProcesses++;
+                currentTime += processBurstTime;
+            } else {
+                // If no process is ready to execute, increment time
+                currentTime++;
+            }
+        }
+    
+        // Correctly calculate waiting and turnaround times
+        for (int i = 0; i < numberOfProcess; i++) {
+            int finish = finishTime.get(i);
+            int arrival = arrivalTime.get(i);
+            int burst = burstTime.get(i);
+    
+            int turnaroundTime = finish - arrival;
+            int waitingTime = turnaroundTime - burst;
+    
+            //System.out.println("| " + processName.get(i) + " | " + arrivalTime.get(i) + " | " + burstTime.get(i) + " | " + priority.get(i) + " | " + finish + " | " + turnaroundTime + " | " + waitingTime + " |");
+        }
+    
+        // Calculate average times
+        double avgTurnaroundTime = 0;
+        double avgWaitingTime = 0;
+        for (int i = 0; i < numberOfProcess; i++) {
+            int turnaroundTime = finishTime.get(i) - arrivalTime.get(i);
+            int waitingTime = turnaroundTime - burstTime.get(i);
+            avgTurnaroundTime += turnaroundTime;
+            avgWaitingTime += waitingTime;
+        }
+    
+        avgTurnaroundTime /= numberOfProcess;
+        avgWaitingTime /= numberOfProcess;
+    
+        //System.out.println("Average Turnaround Time: " + avgTurnaroundTime);
+        //System.out.println("Average Waiting Time: " + avgWaitingTime);
     }
 }
